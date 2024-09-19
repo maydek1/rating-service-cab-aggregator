161a5e285959c453f022a214f4528452cc09a110
maydek1 <maydekcraft@gmail.com>
2024-09-19 21:24:48 +0300

    chore: initialize project and update application.yml

---
+++ src/main/java/com/software/modsen/ratingservicecabaggregator/RatingServiceCabAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.software.modsen.ratingservicecabaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceCabAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingServiceCabAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/software/modsen/ratingservicecabaggregator/RatingServiceCabAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.modsen.ratingservicecabaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceCabAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
