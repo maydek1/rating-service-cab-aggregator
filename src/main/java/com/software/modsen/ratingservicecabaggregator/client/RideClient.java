c9eabccba223996aacf9cfed801923cb4afce039
maydek1 <maydekcraft@gmail.com>
2024-10-03 09:44:34 +0300

    feat(risillence4j): changed circuit breaker

--- src/main/java/com/software/modsen/ratingservicecabaggregator/client/RideClient.java
+++ src/main/java/com/software/modsen/ratingservicecabaggregator/client/RideClient.java
@@ -1,21 +1,33 @@
 package com.software.modsen.ratingservicecabaggregator.client;
 
-import com.software.modsen.ratingservicecabaggregator.config.FeignConfig;
 import com.software.modsen.ratingservicecabaggregator.dto.request.DriverRatingRequest;
 import com.software.modsen.ratingservicecabaggregator.dto.response.DriverResponse;
 import com.software.modsen.ratingservicecabaggregator.dto.response.RideResponse;
+import com.software.modsen.ratingservicecabaggregator.exception.ServiceUnavailableException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "${ride-service.name}", path = "${ride-service.path}", configuration = FeignConfig.class)
+@FeignClient(name = "${ride-service.name}", path = "${ride-service.path}")
 public interface RideClient {
+
     @GetMapping("/{id}")
-    @CircuitBreaker(name = "rideService")
+    @CircuitBreaker(name = "rideService", fallbackMethod = "handleGetRideFallback")
     ResponseEntity<RideResponse> getRide(@PathVariable Long id);
 
     @PostMapping("/driver")
-    @CircuitBreaker(name = "rideService")
+    @CircuitBreaker(name = "driverService", fallbackMethod = "handleUpdateRatingFallback")
     ResponseEntity<DriverResponse> updateRating(@RequestBody DriverRatingRequest driverRatingRequest);
+
+    default ResponseEntity<RideResponse> handleGetRideFallback(Long id, Throwable throwable) {
+        throw new ServiceUnavailableException("Сервис поездок недоступен");
+    }
+
+    default ResponseEntity<DriverResponse> handleUpdateRatingFallback(DriverRatingRequest request, Throwable throwable) {
+        throw new ServiceUnavailableException("Сервис водителей недоступен");
+    }
 }
